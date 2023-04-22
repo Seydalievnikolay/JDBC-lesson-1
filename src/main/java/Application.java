0eb5ab62a2135ae6665135c593a9b23b7428ae8e
Seydalievnikolay <nikolay-seydalie@yandex.ru>
2023-04-22 20:26:40 +0700

    43 строка класса Application не могу задать город.

--- src/main/java/Application.java
+++ src/main/java/Application.java
@@ -8,8 +8,6 @@ import java.util.List;
 
 public class Application {
 
-
-
     public static void main(String[] args) throws SQLException {
         final String user = "postgres";
         final String password = "nimbus2001";
